f61f03c2f2673e846b43878534309413396dc01f
Rajps55 <hackinggm69@gmail.com>
2024-09-25 11:38:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ if len(LOG_CHANNEL) == 0:
     exit()
 else:
     LOG_CHANNEL = int(LOG_CHANNEL)
-IS_FSUB = is_enabled('IS_FSUB', True)
+IS_FSUB = is_enabled('IS_FSUB', False)
 
 # support group
 SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002155777503')
